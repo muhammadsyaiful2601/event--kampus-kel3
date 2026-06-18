e242daadd8a4936b5dfb0953ec4aaaa5b815c7d3
Rafi Pratama <rafirizkypratama17@gmail.com>
2026-06-18 11:51:26 +0700

    login dan register page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 Route::get('/', function () {
-    return view('index');
+    return view('auth.login');
 });
+
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+
+
+Route::post('/login', [AuthController::class, 'login']);
+
+// Registration
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
